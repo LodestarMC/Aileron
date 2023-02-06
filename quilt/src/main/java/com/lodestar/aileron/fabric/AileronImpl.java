71645cd57e5a47383c8056c9790fe171a92a43dd
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-02-06 16:00:20 +0100

    Update AileronImpl.java

--- quilt/src/main/java/com/lodestar/aileron/fabric/AileronImpl.java
+++ quilt/src/main/java/com/lodestar/aileron/fabric/AileronImpl.java
@@ -4,10 +4,17 @@ import com.lodestar.aileron.AileronConfigInfo;
 import com.lodestar.aileron.fabriclike.AileronFabricLike;
 import com.lodestar.aileron.fabriclike.AileronFabricLikeConfig;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
+import net.fabricmc.fabric.api.entity.event.v1.FabricElytraItem;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.quiltmc.loader.api.QuiltLoader;
 
 public class AileronImpl {
@@ -31,7 +38,17 @@ public class AileronImpl {
 
         return configInfo;
     }
+    
+    public static boolean wearingElytra(Player player) {
+        ItemStack stack = player.getItemBySlot(EquipmentSlot.CHEST);
+        if (stack.is(Items.ELYTRA) && ElytraItem.isFlyEnabled(stack))
+            return true;
+        return EntityElytraEvents.ALLOW.invoker().allowElytraFlight(player);
+    }
 
+    public static boolean isElytra(ItemStack stack) {
+        return stack.is(Items.ELYTRA) || stack.getItem() instanceof FabricElytraItem;
+    }
 
     public static boolean isModInstalled(String modId) {
         return QuiltLoader.isModLoaded(modId);
